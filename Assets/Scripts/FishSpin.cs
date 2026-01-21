b688cb21def68861b2f7775b777c3eaad3c82181
miquelan <miquelan@OA-S141-11>
2026-01-21 10:29:29 -0500

    Turned the fish into an array of sprites, added colour randomization in conjunction with sprite randomization

---
+++ Assets/Scripts/FishSpin.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class FishSpin : MonoBehaviour
+{
+    public Transform spin;
+    public float t;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        if (t > 1)
+        {
+            t = 0;
+        }
+    }
+}
--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -5,20 +5,25 @@ public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color col;
+    public Sprite[] fish;
+    public int randomNumber;
+        
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //PickARandomColor();
+        PickARandomColor();
+        PickARandomSprite();
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Keyboard.current.anyKey.wasPressedThisFrame == true)
-        //{
-        //    //PickARandomColor();
-        //}
+        if (Keyboard.current.anyKey.wasPressedThisFrame == true)
+        {
+           PickARandomSprite();
+           PickARandomColor();
+        }
 
         //get the mouse position
         Vector2 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
@@ -37,6 +42,13 @@ public class SpriteChanger : MonoBehaviour
     }
     void PickARandomColor()
     {
-        spriteRenderer.color = Random.ColorHSV();
+        col = Random.ColorHSV();
+    }
+
+    void PickARandomSprite()
+    {
+        //get a random number 0-2 and use that to choose a sprite 
+        randomNumber = Random.Range(0, fish.Length);
+        spriteRenderer.sprite = fish[randomNumber];
     }
 }
