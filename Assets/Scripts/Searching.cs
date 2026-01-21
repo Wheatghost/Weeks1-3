2859485fe23d4724d70a10c68a0b652ecd15a66b
miquelan <miquelan@OA-S141-11>
2026-01-21 09:57:54 -0500

    Fish changes colour on bounce now

--- Assets/Scripts/Searching.cs
+++ Assets/Scripts/Searching.cs
@@ -3,6 +3,7 @@ using static UnityEditor.PlayerSettings;
 
 public class Searching : MonoBehaviour
 {
+    public SpriteRenderer spriteRenderer;
     public Vector2 start;
     public Vector2 end;
     public float t;
@@ -25,7 +26,12 @@ public class Searching : MonoBehaviour
             start = end;
             end.x = Random.Range(-9, 9);
             end.y = Random.Range(-5, 5);
+            PickARandomColor();
         }
         transform.position = Vector2.Lerp(start, end, t);
+    }
+    void PickARandomColor()
+    {
+        spriteRenderer.color = Random.ColorHSV();
     }
 }
